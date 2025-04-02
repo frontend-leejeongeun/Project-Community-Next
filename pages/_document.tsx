7fb54162ce770fcb5450aa3fd0896ab516ada1dd
frontenddeveloper-leejeongeun <49803637+frontend-leejeongeun@users.noreply.github.com>
2025-04-02 23:05:14 +0900

    :-update 폰트 내부에셋 처리

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,18 +4,7 @@ export default function Document() {
     return (
         <Html lang="ko">
             <Head>
-                <link
-                    rel="preload"
-                    as="style"
-                    href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/variable/pretendardvariable.css"
-                    // @ts-ignore
-                    onLoad="this.onload=null;this.rel='stylesheet'"
-                />
-                <noscript
-                    dangerouslySetInnerHTML={{
-                        __html: `<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/variable/pretendardvariable.css" />`,
-                    }}
-                />
+                <title>Community</title>
             </Head>
             <body className="font-sans antialiased">
                 <Main />
